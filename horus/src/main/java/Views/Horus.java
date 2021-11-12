a614e0c0b0d1f03d83f5d2ac72e3bfdf1b5dfd60
ogabrielalves <gabriel.silva@bandtec.com.br>
2021-11-12 11:23:51 -0300

    Atualização da função dos logs.

--- horus/src/main/java/Views/Horus.java
+++ horus/src/main/java/Views/Horus.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -431,9 +432,11 @@ public class Horus extends javax.swing.JFrame {
                     }
                 }
                 if (gpuTemp < 80.5) {
-                    String temperaturaGPU = "Temperatura da GPU: " + gpuTemp;
-                    LocalDateTime dataHora = LocalDateTime.now();
-                    String dadosLog = temperaturaGPU + "\nData e Hora: " + dataHora + "\n";
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                    String dataHora = dtf.format(LocalDateTime.now()).toString();
+                    String usoRam = Conversor.formatarBytes(memRam.getEmUso());
+                    String freqCPU = Conversor.formatarBytes(processador.getFrequencia());
+                    String dadosLog = String.format("Data e Hora: %s\nTemperatura da GPU: %sºC\nUso da RAM: %s\nFrequência CPU: %s\n\n", dataHora, gpuTemp, usoRam, freqCPU);
                     Log.criarLog("Erro.txt", dadosLog);
                 }
             }
