a1e762f6211fe14a51de71d9ab441d9c581bde41
ogabrielalves <gabriel.silva@bandtec.com.br>
2021-12-02 00:52:23 -0300

    Atualização da API e do DER.

--- horus/src/main/java/Views/Slack.java
+++ horus/src/main/java/Views/Slack.java
@@ -19,16 +19,13 @@ import org.json.JSONObject;
 public class Slack {
 
     private static HttpClient client = HttpClient.newHttpClient();
-<<<<<<< HEAD
-    private static final String URL = "https://hooks.slack.com/services/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";
-=======
+
     private static final String URL = "https://hooks.slack.com/services/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";
->>>>>>> 17727c8cfea4501d9813261350f23eb06c9c6604
 
     public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
 
         HttpRequest request = HttpRequest.newBuilder(
-                        URI.create(URL))
+                URI.create(URL))
                 .header("accept", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
                 .build();
